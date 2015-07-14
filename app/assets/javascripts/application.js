e015c4823533f8eb7cb1ce3aaedbc53a0b1dbf6a
Jacob Smergel <jacob.smergel@gmail.com>
2015-07-14 10:39:01 -0400

    heroku changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,20 @@
+// Coffee
+$(document).on 'page:change', ->
+ if window._gaq?
+  _gaq.push ['_trackPageview']
+ else if window.pageTracker?
+  pageTracker._trackPageview()
+
+// Javascript
+$(document).on('page:change', function() {
+ if (window._gaq != null) {
+  return _gaq.push(['_trackPageview']);
+ } else if (window.pageTracker != null) {
+  return pageTracker._trackPageview();
+ }
+});
+
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
